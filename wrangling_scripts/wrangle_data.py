ee588b3a37bcc636b6632e494320418badd7200f
unknown <muellermax1985@gmail.com>
2021-05-24 23:29:28 +0200

    Update: changed plot colors

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -106,7 +106,7 @@ def return_figures():
     layout_one = dict(title='Generation in Germany during the last 14 days',
                       xaxis=dict(title='Date'),
                       yaxis=dict(title='Net Generation (MW)'),
-                      colorway = ['#fefe22', '#1e90ff', '#8b4513', '#191970', '#696969', '#778899', '#87cefa', '#ff0038', '#6495ed', '#6ca0dc', '#006400', '#32cd32', '#808080', '#778899', '#77dd77', '#c0362c'],
+                      colorway = ['#c0362c', '#77dd77', '#778899', '#808080', '#32cd32', '#006400', '#6ca0dc', '#6495ed', '#ff0038', '#87cefa', '#778899', '#696969', '#191970', '#8b4513', '#1e90ff', '#fefe22'],
                       # gelb, hellblau, braun, dunkelblau, dunkelgrau, graublau, hellblau (Hydropumpe), rot, mittelblau, mittelblau#2, dunkelgrün Waste, grün, grau (rest), grau Öl, grün, braun
                       plot_bgcolor = '#E8E8E8'
                       )
